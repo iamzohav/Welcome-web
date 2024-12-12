fdcbc81c9e053d1e62752fb72d3e42c39f13f9a0
iamzohav <bjohrabanu@gmail.com>
2024-12-12 22:26:57 +0530

    Adding css and js files

---
+++ web.js
@@ -0,0 +1,4 @@
+// JavaScript to display a message when the button is clicked
+document.getElementById('clickButton').addEventListener('click', function() {
+    document.getElementById('message').innerText = "You clicked the button!";
+});
